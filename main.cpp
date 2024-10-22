120ed1ec737356a834ed658398391b6be2c881b6
ailsonguedes <ailsonsixseven@gmail.com>
2024-10-22 16:16:33 -0300

    feat(main): app first version

--- main.cpp
+++ main.cpp
@@ -1,19 +1,6 @@
-#include <QApplication>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QWidget>
-#include <QLineEdit>
-#include <QLabel>
-#include <memory>
 #include "src/genpass.hpp"
 
 int main(){
-    QApplication app(argc, argv);
-
-    // Criar widget principal
-    QWidget window;
-    window.setWindowTitle("Gerador de Senhas");
-
     auto g = std::make_unique<GenPass>();
     std::cout << g->gen_pass(32) << '\n';
 
